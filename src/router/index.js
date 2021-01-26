f8da8f29ed7b48819545e16d8c5f2154c5576b1e
Luiza <luitays@mail.ru>
2021-01-26 10:14:47 +0300

    added base templates for new routes and views

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,9 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+    meta: {
+      layout: 'main',
+    },
   },
   {
     path: '/login',
@@ -19,12 +22,60 @@ const routes = [
     component: () => import('@/views/Login.vue'),
   },
   {
+    path: '/register',
+    name: 'register',
+    meta: {
+      layout: 'empty',
+    },
+    component: () => import('@/views/Register.vue'),
+  },
+  {
     path: '/categories',
     name: 'categories',
     meta: {
       layout: 'main',
     },
     component: () => import('@/views/Categories.vue'),
+  },
+  {
+    path: '/detail-record',
+    name: 'detail-record',
+    meta: {
+      layout: 'main',
+    },
+    component: () => import('@/views/DetailRecord.vue'),
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {
+      layout: 'main',
+    },
+    component: () => import('@/views/History.vue'),
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {
+      layout: 'main',
+    },
+    component: () => import('@/views/Planning.vue'),
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {
+      layout: 'main',
+    },
+    component: () => import('@/views/Profile.vue'),
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {
+      layout: 'main',
+    },
+    component: () => import('@/views/Record.vue'),
   },
 ];
 
