19ce9a05240b7b2abef324ce19061f33cb53bad4
Luiza <luitays@mail.ru>
2021-01-27 22:35:54 +0300

    added vuelidate, validaton for register form

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import Vue from 'vue';
 import App from './App.vue';
-import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
+import Vuelidate from 'vuelidate';
 // filters
 import dateFilter from "@/filters/date.filter";
 
+import './registerServiceWorker';
 import 'materialize-css/dist/js/materialize.min';
 
+Vue.use(Vuelidate);
 Vue.filter('date', dateFilter);
 
 Vue.config.productionTip = false;
